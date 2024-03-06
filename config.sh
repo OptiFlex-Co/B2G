a49e03863aa65209c00cf4603fa866d3e40fc662
Abhi <155116505+Abhi-Flex23@users.noreply.github.com>
2024-03-06 16:06:00 +0100

    Connecting to OptiOS UI

--- config.sh
+++ config.sh
@@ -30,7 +30,7 @@ case `uname` in
 	exit -1
 esac
 
-GITREPO=${GITREPO:-"https://github.com/capyloon/manifests"}
+GITREPO=${GITREPO:-"https://github.com/OptiFlex-Co/Manifests"}
 BRANCH=${BRANCH:-master}
 
 while [ $# -ge 1 ]; do
